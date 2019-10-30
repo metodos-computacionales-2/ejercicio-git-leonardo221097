2f34f0e2a11f903bea333595a2bbcbdc60e83656
leonardo221097 <leoalca22@gmail.com>
2019-10-30 20:50:31 +0000

    se insertan los 3 numeros enteros

---
+++ archivo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+
+int main(void)
+{
+ std::cout<<"ingrese el valor del entero 1"<<std::endl;
+    int N1;
+    // se pide el valor de N1
+    std::cin>>N1;
+   std::cout<<"ingrese el valor del entero 2"<<std::endl;
+    int N2;
+    // se pide el valor de N
+    std::cin>>N2;
+     std::cout<<"ingrese el valor del entero 3"<<std::endl;
+    int N3;
+    // se pide el valor de N
+    std::cin>>N3;
+
+}
+
+ 
